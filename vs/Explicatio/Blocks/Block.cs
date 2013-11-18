34835f85fd2af5227dd38c92d13f4968944dda33
dex3r <dex3r.dx@gmail.com>
2013-11-19 00:02:26 +0100

    Block

---
+++ vs/Explicatio/Blocks/Block.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Explicatio.Worlds;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Explicatio.Blocks
+{
+    public class Block
+    {
+        #region Static
+
+        private static Block[] blocks = new Block[256];
+        public static Block[] Blocks
+        {
+            get { return Block.blocks; }
+        }
+
+        static Block()
+        {
+            blocks[0] = null;
+            blocks[1] = new Block(1, "Grass");
+            blocks[2] = new Block(2, "Road");
+        }
+
+        #endregion
+        #region Non-static
+
+        private byte id;
+        public byte Id
+        {
+            get { return id; }
+        }
+
+        private String name;
+        public String Name
+        {
+            get { return name; }
+        }
+
+        private Texture2D texture;
+        /// <summary>
+        /// Podstawowa tekstura dla tego bloku
+        /// </summary>
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+
+        public Block(byte id, String name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Block(byte id, String name, Texture2D texture) : this(id, name)
+        {
+            this.texture = texture;
+        }
+
+        #endregion
+    }
+}
---
+++ vs/Explicatio/Blocks/IMetaTexture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Explicatio.Blocks
+{
+    public class IMetaTexture
+    {
+        /// <summary>
+        /// Zwraca teksturę w zależności do metadaty
+        /// </summary>
+        /// <param name="metadata"></param>
+        /// <returns>Tekstura w zależności do metadaty</returns>
+        public Texture2D GetTextureByMetadata(byte metadata);
+    }
+}
