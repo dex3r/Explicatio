81b67792983010b13169f6d67e79157c144c7196
Lotny <99999deuter@gmail.com>
2013-11-19 18:12:26 +0100

    Raw Camera

--- vs/Explicatio/Main/Main.cs
+++ vs/Explicatio/Main/Main.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.GamerServices;
+
 using Explicatio.Rendering;
 using Explicatio.Entities;
 
@@ -15,6 +17,8 @@ namespace Explicatio.Main
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
+        private Camera camera; float zoom = 1;
+
 
         public Main()
             : base()
@@ -31,7 +35,9 @@ namespace Explicatio.Main
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
             Textures.Load(this.Content);
+            camera = new Camera(new Viewport(0,0,GraphicsDevice.Viewport.Width,GraphicsDevice.Viewport.Height));
         }
 
         protected override void UnloadContent()
@@ -45,19 +51,25 @@ namespace Explicatio.Main
             {
                 Exit();
             }
-
+            //TODO Przeniść w fajne miejsce
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                zoom -= 0.005f;
+            else if (Mouse.GetState().RightButton == ButtonState.Pressed)
+                zoom += 0.005f;
+            camera.Update(gameTime, 0, new Vector2(GraphicsDevice.Viewport.Width/2, GraphicsDevice.Viewport.Height/2), zoom);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            spriteBatch.Begin();
+            GraphicsDevice.Viewport = camera.View;
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone, null, camera.Transform);
             GlobalRenderer.Draw(spriteBatch, gameTime);
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
     }
+
 }
