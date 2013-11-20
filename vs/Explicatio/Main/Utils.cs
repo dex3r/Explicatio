ee92e2b1ba6f3ddccf0dee6595a272d73e3feda8
Muchaszewski <99999deuter@gmail.com>
2013-11-20 10:30:46 +0100

    frame for text class

--- vs/Explicatio/Main/Utils.cs
+++ vs/Explicatio/Main/Utils.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace Explicatio.Main
 {
@@ -12,6 +14,39 @@ namespace Explicatio.Main
         public static Rectangle RotateRectangle()
         {
             throw new NotImplementedException();
+        }
+    }
+    /// <summary>
+    /// NIE DZIAŁA JESZCZE!!
+    /// </summary>
+    public class Text
+    {
+        private static string textString;
+        public string TextString
+        {
+            get { return textString; }
+            set { textString = value;}
+        }
+        public Vector2 Postion { get; private set; }
+        private static SpriteFont font;
+        public SpriteFont Font 
+        {
+            get { return font; }
+            set { font = value;}
+        }
+        public Color TextColor { get; private set;} 
+        public Text(Vector2 position, ContentManager cm)
+        {
+            textString = "";
+            Postion = position;
+            Font = null;
+            //Font = cm.Load<SpriteFont>("fonts/Courier New");
+            TextColor = Color.Black;
+        }
+        public void Draw(SpriteBatch batch, GameTime time)
+        {
+            batch.DrawString(Font, TextString, Postion, TextColor,
+        0, Font.MeasureString(TextString) / 2, 1.0f, SpriteEffects.None, 0.5f);
         }
     }
 }
