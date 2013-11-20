6b8f7bbfa164644161484568fa62e817f9a288bb
dex3r <dex3r.dx@gmail.com>
2013-11-20 21:33:43 +0100

    Testy wydajności

--- vs/Explicatio/Rendering/ChunkRenderer.cs
+++ vs/Explicatio/Rendering/ChunkRenderer.cs
@@ -13,13 +13,17 @@ namespace Explicatio.Rendering
     {
         public static void DrawChunk(SpriteBatch batch, Chunk chunk)
         {
+            Vector2 v;
             for (ushort x = 0; x < Chunk.CHUNK_SIZE; x++)
             {
-                for (ushort y = 0; y < Chunk.CHUNK_SIZE; y++)
+                for (ushort y = 0; y < Chunk.CHUNK_SIZE; )
                 {
                     // Wszystkie bloki są obracane w prawo 0 45* aby stworzyć wrażenie izometrii
+                    v = new Vector2((Chunk.CHUNK_SIZE - y + x) * 32, (x + y) * 16);
+                    
                     //batch.Draw(Block.Blocks[chunk[x, y]].Texture, new Vector2((Chunk.CHUNK_SIZE - y + x) * 32 + ((chunk.WorldObj.ChunksInRow - chunk.Y + chunk.X) * Chunk.CHUNK_SIZE * 32), (x + y) * 16 + ((chunk.X + chunk.Y) * 16 * Chunk.CHUNK_SIZE)), Color.White);
-                    batch.Draw(Block.Blocks[chunk[x, y]].Texture, new Vector2((Chunk.CHUNK_SIZE - y + x) * 32, (x + y) * 16) , Color.White);
+                    batch.Draw(Block.Blocks[chunk[x, y]].Texture, v, Color.White);
+                    y++;
                 }
             }
             //TODO: dodać renderowanie pojazdów i budynków
