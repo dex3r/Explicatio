2aeb3589c54ed6b0ede88c25997d51ed25238fc1
dex3r <dex3r.dx@gmail.com>
2013-12-14 18:35:48 +0100

    Finished drawing fixes

--- vs/Explicatio/Rendering/GlobalRenderer.cs
+++ vs/Explicatio/Rendering/GlobalRenderer.cs
@@ -18,7 +18,7 @@ namespace Explicatio.Rendering
         public static void Draw(GameTime time)
         {
             GameMain.SpriteBatch.End();
-            World world = Main.GameMain.Instance.CurrentWorld;
+            World world = Main.GameMain.CurrentWorld;
             Chunk c;
             for (int x = 0; x < world.ChunksInRow; x++)
             {
@@ -33,49 +33,41 @@ namespace Explicatio.Rendering
             }
             GameMain.BeginNormalDrawing();
             GameMain.SpriteBatch.GraphicsDevice.Clear(Color.CornflowerBlue);
-            DrawWorld(Main.GameMain.Instance.CurrentWorld);
+            DrawWorld(Main.GameMain.CurrentWorld);
         }
 
         public static void DrawWorld(World world)
         {
-            GameMain.Instance.LastDrawedChunksCount = 0;
+            GameMain.LastDrawedChunksCount = 0;
             Chunk c;
             Vector2 v;
             // const dla wydajności
             const int o1 = Chunk.CHUNK_SIZE * 32;
             const int o2 = Chunk.CHUNK_SIZE * 16;
-            // pozycja chunka do wyrenderowania
+            // Pozycja chunka do wyrenderowania
             int mx, my;
-            float relativeX = Camera.Transform.Translation.X * (1 / Camera.Zoom);
-            float relativeY = Camera.Transform.Translation.Y * (1 / Camera.Zoom);
-            float startY = ((relativeX / o1) - (relativeY / o2) - world.ChunksInRow) / -2;
-            float startX = (relativeY / o2) - startY;
-            for (int x = (int)Math.Max(0, Math.Floor(startX)); x < world.ChunksInRow; x++)
-            {
-                for (int y = (int)Math.Max(0, Math.Floor(startY)); y < world.ChunksInRow; y++)
-                {
-                    c = world.GetChunk(x, y);
-                    mx = (world.ChunksInRow - y + x) * o1;
-                    my = (x + y) * o2;
-                    v = new Vector2(mx, my);
-                    //Text.Draw(v.X + " " + v.Y, v, Color.Azure, 0.4f);
-                    GameMain.SpriteBatch.Draw(c.RenderTarget, v, Color.White);
-                    GameMain.Instance.LastDrawedChunksCount++;
-                }
-            }
-
-            GameMain.SpriteBatch.End();
-            GameMain.BeginNormalDrawing();
-
-            //!? Temp code
+            // Pozycja pierwszego widocznego pixela
+            float startscreenX = -Camera.Transform.Translation.X * (1 / Camera.Zoom);
+            float startscreenY = -Camera.Transform.Translation.Y * (1 / Camera.Zoom);
+            // Pozycja ostatniego widocznego pixela
+            float endscreenX = startscreenX + Main.GameMain.SpriteBatch.GraphicsDevice.Viewport.Width * (1 / Camera.Zoom);
+            float endscreenY = startscreenY + Main.GameMain.SpriteBatch.GraphicsDevice.Viewport.Height * (1 / Camera.Zoom);
             for (int x = 0; x < world.ChunksInRow; x++)
             {
                 for (int y = 0; y < world.ChunksInRow; y++)
                 {
+                    c = world.GetChunk(x, y);
                     mx = (world.ChunksInRow - y + x) * o1;
                     my = (x + y) * o2;
+                    // Rysowanie tylko widocznych chunków
+                    if (mx + c.RenderTarget.Width < startscreenX - c.RenderTarget.Height || my + c.RenderTarget.Height < startscreenY || mx > endscreenX || my > endscreenY)
+                    {
+                        continue;
+                    }
+                    //Pozycja chunka do narysowania
                     v = new Vector2(mx, my);
-                    Primitives2D.DrawRectangle(GameMain.SpriteBatch, new Rectangle(mx, my, Chunk.CHUNK_SIZE * 64 + 64, Chunk.CHUNK_SIZE * 32 + 16), Color.Red, 5f);
+                    GameMain.SpriteBatch.Draw(c.RenderTarget, v, Color.White);
+                    GameMain.LastDrawedChunksCount++;
                 }
             }
         }
