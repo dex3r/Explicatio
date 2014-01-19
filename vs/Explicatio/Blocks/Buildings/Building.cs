30773ba7d0cb8fe77d9ba4a0ba8645508ccb8ad4
Mucahszewski <99999deuter@gmail.com>
2014-01-19 12:02:58 +0100

    Init Buildings

---
+++ vs/Explicatio/Blocks/Buildings/Building.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Explicatio.Blocks
+{
+    public class Building
+    {
+
+        private ushort x;
+        public ushort X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private ushort y;
+        public ushort Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        private Point chunkPosition;
+        public Point ChunkPosition
+        {
+            get { return chunkPosition; }
+            set { chunkPosition = value; }
+        }
+
+        public Building(Point chunkPosition, ushort x, ushort y)
+        {
+            this.chunkPosition = chunkPosition;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ vs/Explicatio/Blocks/Buildings/BuildingContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using Explicatio.Worlds;
+
+namespace Explicatio.Blocks
+{
+    public static class BuildingContainer
+    {
+        public static SortedList<BuildingEnum, Building> container = new SortedList<BuildingEnum, Building>();
+
+        public static void AddBuilding(World world, int chunkX, int chunkY, ushort x, ushort y, BuildingEnum building)
+        {
+            container.Add(building,new Building(new Point(chunkX,chunkY),x,y);
+            world.GetChunk(chunkX, chunkY)[x, y] = (byte)building;
+        }
+    }
+}
