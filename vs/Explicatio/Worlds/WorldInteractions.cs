c31ac0b9528569a3987e7bc22af31626599f5841
Mucahszewski <99999deuter@gmail.com>
2014-04-11 15:41:54 +0200

    fix

---
+++ vs/Explicatio/Worlds/WorldInteractions.cs
@@ -0,0 +1,23 @@
+﻿using Explicatio.Controls;
+using Explicatio.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicatio.Worlds
+{
+    public static class WorldInteractions
+    {
+        public static void Mouse()
+        {
+            int[] CurrentBlock = GameMain.CurrentWorld.RelativeGetBlock(MyMouse.XRelative, MyMouse.YRelative);
+        }
+
+        public static void Keyboard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
