7b7aa4d9da0634557ae9872b767e16e60b9d40a0
Mucahszewski <99999deuter@gmail.com>
2014-05-16 00:42:49 +0200

    RoadEnum BinaryFlags and Road

---
+++ vs/Explicatio/Blocks/BlockFlags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicatio.Blocks
+{
+    [Flags]
+    enum RoadMeta
+    {
+        Up = 1,
+        Down = 2,
+        Left = 4,
+        Right = 8,
+
+    }
+}
---
+++ vs/Explicatio/Blocks/BlockList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicatio.Blocks
+{
+    public static class BlockList
+    {
+        public static Block Grass = new Block(1, "Grass");
+        public static BlockRoad Road = new BlockRoad(2, "Road");
+    }
+}
